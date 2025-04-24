84776aad245cf97c306baee82ca0e70e68dc6d58
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-04-24 20:54:56 +0900

    feat: layout, Header 적용

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex items-center justify-center h-16 bg-primary-blue text-white">
+      <h1 className="text-2xl font-bold">Weebur Assignment</h1>
+    </header>
+  );
+}
