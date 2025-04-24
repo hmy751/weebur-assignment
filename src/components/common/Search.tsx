ba8674c7929d914eaf480caf4b147e516739b4b8
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-04-24 23:59:47 +0900

    feat: 검색 필터, useForm 및 쿼리 적용

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -1,25 +1,37 @@
 "use client";
 
-import React, { useState } from "react";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import { useForm, FieldValues } from "react-hook-form";
 
 export default function Search() {
-  const [searchQuery, setSearchQuery] = useState("");
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
 
-  const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSearch = (data: FieldValues) => {
+    const term = data.searchQuery;
+
+    if (term) {
+      params.set("search", term);
+    } else {
+      params.delete("search");
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
   };
 
   return (
     <form
-      onSubmit={handleSearch}
+      onSubmit={handleSubmit(handleSearch)}
       className="flex items-center w-full bg-white border border-gray-300 rounded-full shadow-sm"
     >
       <span className="pl-4 text-gray-400">Q</span>
 
       <input
         type="text"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        {...register("searchQuery")}
         placeholder="찾고 싶은 상품을 검색해보세요"
         className="flex-1 px-4 py-2 text-gray-700 placeholder-gray-400 bg-transparent outline-none"
       />
