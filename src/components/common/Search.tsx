d87e28e54bf30779508011a6824dc3eb9256ab6c
함명연 <hammyeong-yeon@hammyeong-yeon-ui-MacBookPro.local>
2025-04-25 21:54:31 +0900

    refactor: Search 컴포넌트 상태관리 로직 분리

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -1,37 +1,17 @@
-"use client";
+import { InputHTMLAttributes, Ref, forwardRef } from "react";
 
-import { useSearchParams, useRouter, usePathname } from "next/navigation";
-import { useForm, FieldValues } from "react-hook-form";
-
-export default function Search() {
-  const { register, handleSubmit } = useForm();
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const params = new URLSearchParams(searchParams);
-
-  const handleSearch = (data: FieldValues) => {
-    const term = data.searchQuery;
-
-    if (term) {
-      params.set("search", term);
-    } else {
-      params.delete("search");
-    }
-
-    router.push(`${pathname}?${params.toString()}`);
-  };
-
+export default forwardRef(function Search(
+  { ...props }: InputHTMLAttributes<HTMLInputElement>,
+  ref: Ref<HTMLInputElement>
+) {
   return (
-    <form
-      onSubmit={handleSubmit(handleSearch)}
-      className="flex items-center w-full bg-white border border-gray-300 rounded-full shadow-sm"
-    >
+    <div className="flex items-center w-full bg-white border border-gray-300 rounded-full shadow-sm">
       <span className="pl-4 text-gray-400">Q</span>
 
       <input
+        ref={ref}
         type="text"
-        {...register("searchQuery")}
+        {...props}
         placeholder="찾고 싶은 상품을 검색해보세요"
         className="flex-1 px-4 py-2 text-gray-700 placeholder-gray-400 bg-transparent outline-none"
       />
@@ -42,6 +22,6 @@ export default function Search() {
       >
         검색
       </button>
-    </form>
+    </div>
   );
-}
+});
--- src/components/pages/main/SearchSection.tsx
+++ src/components/pages/main/SearchSection.tsx
@@ -1,9 +1,34 @@
+"use client";
+
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useForm, FieldValues } from "react-hook-form";
+
 import Search from "@/components/common/Search";
 
 export default function SearchSection() {
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+
+  const handleSearch = (data: FieldValues) => {
+    const term = data.searchQuery;
+
+    if (term) {
+      params.set("search", term);
+    } else {
+      params.delete("search");
+    }
+
+    router.push(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <section className="w-full">
-      <Search />
+      <form onSubmit={handleSubmit(handleSearch)}>
+        <Search {...register("searchQuery")} />
+      </form>
     </section>
   );
 }
